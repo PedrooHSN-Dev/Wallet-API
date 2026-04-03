4beebf6fc6d353770f704e286b4febdaea6292d4
PedrooHSN <pppedrohenri@gmail.com>
2026-04-03 13:07:20 -0300

    Implement UserDetails in User class

--- src/main/java/com/securewallet/walletapi/domain/User.java
+++ src/main/java/com/securewallet/walletapi/domain/User.java
@@ -5,21 +5,24 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
 
     @Column(name = "external_id", nullable = false, unique = true, updatable = false)
     private UUID externalId;
@@ -38,7 +41,6 @@ public class User {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-
     public User(String fullName, String email, String passwordHash) {
         this.externalId = UUID.randomUUID();
         this.fullName = fullName;
@@ -46,4 +48,27 @@ public class User {
         this.passwordHash = passwordHash;
         this.createdAt = LocalDateTime.now();
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() { return this.passwordHash; }
+
+    @Override
+    public String getUsername() { return this.email; }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
