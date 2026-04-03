d41ea17cea3316ed32a8e99c046f8afde0961dae
PedrooHSN <pppedrohenri@gmail.com>
2026-04-02 21:21:30 -0300

    Add wallets and transactions tables with indices

--- src/main/resources/db/migration/V2__add_wallets_and_transactions.sql
+++ src/main/resources/db/migration/V2__add_wallets_and_transactions.sql
@@ -1,1 +1,18 @@
-
+CREATE TABLE wallets (
+id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+external_id UUID           NOT NULL UNIQUE,
+user_id     BIGINT         NOT NULL UNIQUE REFERENCES users(id),
+balance     NUMERIC(19, 2) NOT NULL DEFAULT 0.00,
+created_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE transactions (
+id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+external_id UUID           NOT NULL UNIQUE,
+wallet_id   BIGINT         NOT NULL REFERENCES wallets(id),
+type        VARCHAR(30)    NOT NULL,
+amount      NUMERIC(19, 2) NOT NULL,
+description VARCHAR(255),
+created_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_transactions_wallet_id  ON transactions(wallet_id);
+CREATE INDEX idx_transactions_created_at ON transactions(created_at);
