4d7d9d815ae6a2df9f1e95ce3a0d256ed4d03bc3
PedrooHSN <pppedrohenri@gmail.com>
2026-04-02 21:14:32 -0300

    Add findByEmail and existsByEmail methods
    Added method to find user by email.

--- src/main/java/com/securewallet/walletapi/repository/UserRepository.java
+++ src/main/java/com/securewallet/walletapi/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.securewallet.walletapi.repository;
-
 import com.securewallet.walletapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+boolean existsByEmail(String email);
 
-    boolean existsByEmail(String email);
+Optional<User> findByEmail(String email);  // ← adicionar este método
 }
