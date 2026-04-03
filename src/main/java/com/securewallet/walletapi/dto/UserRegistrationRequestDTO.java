dc1c130723045d97a92271bd29799b61debc9f8a
PedrooHSN <pppedrohenri@gmail.com>
2026-04-03 13:07:47 -0300

    Update UserRegistrationRequestDTO field validations

--- src/main/java/com/securewallet/walletapi/dto/UserRegistrationRequestDTO.java
+++ src/main/java/com/securewallet/walletapi/dto/UserRegistrationRequestDTO.java
@@ -5,8 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRegistrationRequestDTO(
-        @NotBlank String fullName,
-        @Email @NotBlank String email,
-        @NotBlank @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres") String password
-) {
-}
+        @NotBlank @Size(max = 150) String fullName,
+        @Email @NotBlank @Size(max = 255) String email,
+        @NotBlank @Size(min = 8, max = 255, message = "A senha deve ter no mínimo 8 caracteres") String password
+) {}
